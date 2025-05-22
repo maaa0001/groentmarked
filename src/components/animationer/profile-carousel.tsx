346f18ca6bd1e374fa0c8e00a6d2e87ee614d832
Malthe <maaa0001@stud.kea.dk>
2025-05-22 15:08:04 +0200

    Rettelser og illustrationer

--- src/components/animationer/profile-carousel.tsx
+++ src/components/animationer/profile-carousel.tsx
@@ -73,12 +73,12 @@ export default function ProfileCarousel() {
     <div className="relative w-full max-w-5xl mx-auto overflow-hidden rounded-3xl">
       <div className="bg-[#2A4837] rounded-3xl flex flex-col gap-10 md:flex-row relative">
         {/* Left side - Image */}
-        <div className="md:w-2/5 relative">
-          <div className="h-full">
+        <div className="md:w-2/5  md:h-[490px] relative">
+          <div className="h-full max-h-[490px]">
             <img
               src={profiles[activeIndex].image}
               alt={profiles[activeIndex].name}
-              className="object-cover w-full h-full transition-opacity duration-500"
+              className="object-cover w-full h-full transition-opacity duration-500 rounded-3xl"
             />
           </div>
         </div>
