c386a91018113d2136a4701e2d0679771c0676bb
MariaRose <maan0008@stud.kea.dk>
2025-05-25 10:31:39 +0200

    Opsætning v2

--- src/components/Header/toggle-button.tsx
+++ src/components/Header/toggle-button.tsx
@@ -6,11 +6,7 @@ export function Toggle() {
 
   useEffect(() => {
     const currentPath = window.location.pathname;
-    if (
-      currentPath === "/producent" ||
-      currentPath.startsWith("/producent/") ||
-      currentPath.includes("/log-ind")
-    ) {
+    if (currentPath === "/producent" || currentPath.startsWith("/producent/") || currentPath.includes("/log-ind")) {
       setSelected("producer");
     } else {
       setSelected("guest");
@@ -19,35 +15,16 @@ export function Toggle() {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="relative flex rounded-[20px] border-2 border-MarketLinen-500 p-0.5 w-[200px] h-[43px] ">
-        <a
-          href="/"
-          onClick={() => setSelected("guest")}
-          className={cn(
-            "relative z-10 w-1/2 py-1 pt-2 text-center text-sm md:text-sm transition-colors duration-200 rounded-[20px] cursor-pointer",
-            selected === "guest" ? "text-MarketLinen-500" : "text-MarketLinen-500"
-          )}
-        >
+      <div className="relative flex rounded-[20px] border-[1.2px] border-MarketLinen-500 p-0.5 w-[200px] h-[43px] ">
+        <a href="/" onClick={() => setSelected("guest")} className={cn("relative z-10 w-1/2 py-1 pt-2 text-center text-sm md:text-sm transition-colors duration-200 rounded-[20px] cursor-pointer", selected === "guest" ? "text-MarketLinen-500" : "text-MarketLinen-500")}>
           Gæst
         </a>
 
-        <a
-          href="/producent"
-          onClick={() => setSelected("producer")}
-          className={cn(
-            "relative z-10 w-1/2 py-1 pt-2 text-center text-sm md:text-sm transition-colors duration-200 rounded-[20px] cursor-pointer",
-            selected === "producer" ? "text-MarketLinen-500" : "text-MarketLinen-500"
-          )}
-        >
+        <a href="/producent" onClick={() => setSelected("producer")} className={cn("relative z-10 w-1/2 py-1 pt-2 text-center text-sm md:text-sm transition-colors duration-200 rounded-[20px] cursor-pointer", selected === "producer" ? "text-MarketLinen-500" : "text-MarketLinen-500")}>
           Producent
         </a>
 
-        <div
-          className={cn(
-            "absolute h-full py-1 left-0 right-0 top-0 bottom-0 w-[50%] bg-MarketMidnight-500 border-2 border-MarketLinen-500 rounded-[20px] transition-transform duration-200 ease-in-out",
-            selected === "producer" ? "translate-x-[calc(100%)]" : ""
-          )}
-        />
+        <div className={cn("absolute h-[43px] left-0 right-[-1px] top-[-1px] bottom-0 w-[50%] bg-MarketMidnight-500 border-[1.2px] border-MarketLinen-500 rounded-[20px] transition-transform duration-200 ease-in-out", selected === "producer" ? "translate-x-[calc(100%)]" : "")} />
       </div>
     </div>
   );
